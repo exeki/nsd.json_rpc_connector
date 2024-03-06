0dac8bc066fbc19709b8effe73911ffa5873a0c6
ekazantsev <ekazantsev@ocs.ru>
2024-03-06 14:26:59 +0400

    добавлены перегрузки с классе Connector

--- src/main/java/ru/kazantsev/nsd/json_rpc_connector/Connector.java
+++ src/main/java/ru/kazantsev/nsd/json_rpc_connector/Connector.java
@@ -76,10 +76,11 @@ public class Connector extends ru.kazantsev.nsd.basic_api_connector.Connector {
     /**
      * Выполнить запрос методом get.
      * Позволяет найти объект по ассотиативному массиву с условными операциями.
-     * @param fqn код метакласс искомого объекта
+     *
+     * @param fqn   код метакласс искомого объекта
      * @param query ассоциативный массив для поиска объекта
-     * @param view перечень кодов возвращаемых объектов, может быть null
-     * @param id единтификатор запроса, может быть null
+     * @param view  перечень кодов возвращаемых объектов, может быть null
+     * @param id    единтификатор запроса, может быть null
      * @return результат выполнения запроса, в ключе result будет:
      * либо строка идентификатора найденного объекта, если view был передан как null,
      * либо Map с полями объекта, которые были переданы во view,
@@ -87,29 +88,38 @@ public class Connector extends ru.kazantsev.nsd.basic_api_connector.Connector {
      * либо в ключе error будет описание произошедшей ошибки.
      * ВАЖНО: случится ошибка, если по переданному query было найдено несколько объектов.
      */
-    public RpcResponseDto jsonRpcGet(String fqn, Query query, List<String> view, Long id){
+    public RpcResponseDto jsonRpcGet(String fqn, Query query, List<String> view, Long id) {
         RpcRequestDto.Get dto = new RpcRequestDto.Get(fqn, query);
         dto.setId(id);
         dto.setView(view);
         return sendRequest(dto);
     }
 
+    public RpcResponseDto jsonRpcGet(String fqn, Query query, List<String> view) {
+        return jsonRpcGet(fqn, query, view, null);
+    }
+
+    public RpcResponseDto jsonRpcGet(String fqn, Query query) {
+        return jsonRpcGet(fqn, query, null, null);
+    }
+
     /**
      * Выполнить запрос методом find.
      * Позволяет найти объекты по ассотиативному массиву с условными операциями.
-     * @param fqn код метакласса искомого объекта
-     * @param query ассотиативных массив для поиска объекта
-     * @param view перечень кодов возвращаемых объектов, может быть null
-     * @param limit лимит возвраемыех объектов, может быть null
+     *
+     * @param fqn    код метакласса искомого объекта
+     * @param query  ассотиативных массив для поиска объекта
+     * @param view   перечень кодов возвращаемых объектов, может быть null
+     * @param limit  лимит возвраемыех объектов, может быть null
      * @param offset оффсет при запросе, может быть null
-     * @param id идентификатор запроса, может быть null
+     * @param id     идентификатор запроса, может быть null
      * @return результат выполнения запроса, в ключе result будет:
      * либо массив строк идентификаторов найденных объектов,
      * либо массив Map с полями объектов, коды которых были переданы во view,
      * либо null, если ничего не было найдено,
      * либо в ключе error будет описание произошедшей ошибки.
      */
-    public RpcResponseDto jsonRpcFind(String fqn, Query query, List<String> view, Long limit, Long offset, Long id){
+    public RpcResponseDto jsonRpcFind(String fqn, Query query, List<String> view, Long limit, Long offset, Long id) {
         RpcRequestDto.Find dto = new RpcRequestDto.Find(fqn, query);
         dto.setId(id);
         dto.setView(view);
@@ -118,12 +128,29 @@ public class Connector extends ru.kazantsev.nsd.basic_api_connector.Connector {
         return sendRequest(dto);
     }
 
+    public RpcResponseDto jsonRpcFind(String fqn, Query query, List<String> view, Long limit, Long offset) {
+        return jsonRpcFind(fqn, query, view, limit, offset, null);
+    }
+
+    public RpcResponseDto jsonRpcFind(String fqn, Query query, List<String> view, Long limit) {
+        return jsonRpcFind(fqn, query, view, limit, null, null);
+    }
+
+    public RpcResponseDto jsonRpcFind(String fqn, Query query, List<String> view) {
+        return jsonRpcFind(fqn, query, view, null, null, null);
+    }
+
+    public RpcResponseDto jsonRpcFind(String fqn, Query query) {
+        return jsonRpcFind(fqn, query, null, null, null, null);
+    }
+
     /**
      * Выполняет запрос методом create.
-     * @param fqn код метакласса создаваемого объекта
+     *
+     * @param fqn   код метакласса создаваемого объекта
      * @param attrs ассоциативный массив, содержащий поля создаваемого объекта
-     * @param view коды атрибутов созданного объекта, возвращаемый в ключе result, может быть null
-     * @param id идентификатор запроса, может быть null
+     * @param view  коды атрибутов созданного объекта, возвращаемый в ключе result, может быть null
+     * @param id    идентификатор запроса, может быть null
      * @return результат выполнения запроса, в ключе result может быть:
      * либо идентификатор созданного объекта, если объект был создан и view был null,
      * либо Map с полями созданного объекта, если объект был создан и view был не null.
@@ -136,33 +163,51 @@ public class Connector extends ru.kazantsev.nsd.basic_api_connector.Connector {
         return sendRequest(dto);
     }
 
+    public RpcResponseDto jsonRpcCreate(String fqn, Attrs attrs, List<String> view) {
+        return jsonRpcCreate(fqn, attrs, view, null);
+    }
+
+    public RpcResponseDto jsonRpcCreate(String fqn, Attrs attrs) {
+        return jsonRpcCreate(fqn, attrs, null, null);
+    }
+
     /**
      * Выполняет запрос методом edit.
      * Позволяет отредактировать объект.
-     * @param uuid идентификатор редактируемого объекта
+     *
+     * @param uuid  идентификатор редактируемого объекта
      * @param attrs ассоциативный массив, содержащий значения полей для редактирования
-     * @param view коды атрибутов отредактированного объекта, возвращаемый в ключе result, может быть null
-     * @param id идентификатор запроса, может быть null
+     * @param view  коды атрибутов отредактированного объекта, возвращаемый в ключе result, может быть null
+     * @param id    идентификатор запроса, может быть null
      * @return результат выполнения запроса, в ключе result может быть:
      * либо идентификатор отредактированного объекта, если объект был отредактирован и view был null,
      * либо Map с полями отредактированного объекта, если объект был отредактирован и view был не null,
      * либо в ключе error будет описание произошедшей ошибки.
      */
-    public RpcResponseDto jsonRpcEdit(String uuid, Attrs attrs, List<String> view, Long id){
+    public RpcResponseDto jsonRpcEdit(String uuid, Attrs attrs, List<String> view, Long id) {
         RpcRequestDto.Edit dto = new RpcRequestDto.Edit(uuid, attrs);
         dto.setId(id);
         dto.setView(view);
         return sendRequest(dto);
     }
 
+    public RpcResponseDto jsonRpcEdit(String uuid, Attrs attrs, List<String> view) {
+        return jsonRpcEdit(uuid, attrs, view, null);
+    }
+
+    public RpcResponseDto jsonRpcEdit(String uuid, Attrs attrs) {
+        return jsonRpcEdit(uuid, attrs, null, null);
+    }
+
     /**
      * Выполняет запрос методом edit.
      * Позволяет редактировать объект, не зная его UUID.
-     * @param fqn код метокласса искомого объекта
+     *
+     * @param fqn   код метокласса искомого объекта
      * @param query ассотиативный объект для поиска объекта
      * @param attrs ассоциативный массив, содержащий значения полей для редактирования
-     * @param view коды атрибутов отредактированного объекта, возвращаемый в ключе result, может быть null
-     * @param id идентификатор запроса, может быть null
+     * @param view  коды атрибутов отредактированного объекта, возвращаемый в ключе result, может быть null
+     * @param id    идентификатор запроса, может быть null
      * @return результат выполнения запроса, в ключе result может быть:
      * либо идентификатор отредактированного объекта, если объект был отредактирован и view был null,
      * либо Map с полями отредактированного объекта, если объект был отредактирован и view был не null,
@@ -174,6 +219,14 @@ public class Connector extends ru.kazantsev.nsd.basic_api_connector.Connector {
         dto.setView(view);
         dto.setId(id);
         return sendRequest(dto);
+    }
+
+    public RpcResponseDto jsonRpcEdit(String fqn, Query query, Attrs attrs, List<String> view) {
+        return jsonRpcEdit(fqn, query, attrs, view, null);
+    }
+
+    public RpcResponseDto jsonRpcEdit(String fqn, Query query, Attrs attrs) {
+        return jsonRpcEdit(fqn, query, attrs, null, null);
     }
 
 }
